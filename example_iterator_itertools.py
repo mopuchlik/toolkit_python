8afff6c1bff1331c3483d9d67541eb8c48f7670d
Michal Opuchlik <michal.opuchlik@gmail.com>
2024-06-30 00:50:53 +0200

    how to count length of iterator

--- example_iterator_itertools.py
+++ example_iterator_itertools.py
@@ -10,6 +10,17 @@ print(next(iterator))
 print(next(iterator))
 
 # %%
+# https://stackoverflow.com/questions/3345785/getting-number-of-elements-in-an-iterator-in-python
+iterator = iter(iter_l)
+sum(1 for _ in iterator)
+
+# Also, be aware that the iterator will be exhausted by doing this,
+# and further attempts to use it will see no elements.
+# That's an unavoidable consequence of the Python iterator design.
+# If you want to keep the elements, you'll
+# have to store them in a list or something.
+
+# %%
 x = it.count(10)
 print(next(x))
 print(next(x))
